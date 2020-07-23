4cab8a18988a2f22c21d8e2d63da62cfbae02a10
jakubhenzl5@gmail.com <jakubhenzl5@gmail.com>
2020-07-23 14:57:32 +0200

    oprava skriptu

--- script_server_sambar.R
+++ script_server_sambar.R
@@ -1,21 +1,22 @@
 #oprava germinalnich dat podle checkgenesymbols
 library(HGNChelper)
 library(tidyverse)
+library(SAMBAR)
 
 germ_matrix<-read.csv2("/media/hdc/tmp/PCAWG_somatic_germline/Jakub/data/sign_mat.csv",row.names = 1)
-approved_genes<-checkGeneSymbols(colnames(germ_matrix))
+current_gene_map<-getCurrentHumanMap()
+approved_genes<-checkGeneSymbols(colnames(germ_matrix),map=current_gene_map)
+approved_genes[is.na(approved_genes$Suggested.Symbol), 3] <- approved_genes[is.na(approved_genes$Suggested.Symbol), 1]
 colnames(germ_matrix)<-approved_genes$Suggested.Symbol
-germ_matrix<-germ_matrix[,!is.na(colnames(germ_matrix))]
-
 
 #vektor exon.size ze sambar
-exon<-load("/media/hdc/tmp/PCAWG_somatic_germline/Jakub/data/exon.size.RData")
-approved_exon_size<-checkGeneSymbols(names(exon.size))
 
-
-exon_size_correct<-exon.size
+approved_exon_size<-checkGeneSymbols(names(SAMBAR::exon.size),map=current_gene_map)
+approved_exon_size[is.na(approved_exon_size$Suggested.Symbol),3]<-approved_exon_size[is.na(approved_exon_size$Suggested.Symbol),1]
+table(is.na(approved_exon_size$Suggested.Symbol))
+exon_size_correct<-SAMBAR::exon.size
 names(exon_size_correct)<-approved_exon_size$Suggested.Symbol
-exon_size_correct<-exon_size_correct[!is.na(names(exon_size_correct))]
+
 
 #nacteni drahove matice z gprofileru a kontrola genu pomoci checkgenesymbols
 gprofiler_dataset<-read_csv2("/media/hdc/tmp/PCAWG_somatic_germline/Jakub/gprofiler_dataset_ok.csv.gz")
@@ -23,11 +24,10 @@ gprofiler_dataset<-as.data.frame(gprofiler_dataset)
 rownames(gprofiler_dataset)<-gprofiler_dataset$X1
 gprofiler_dataset<-gprofiler_dataset[,-1]
 
-approved_gprofiler<-checkGeneSymbols(colnames(gprofiler_dataset))
+approved_gprofiler<-checkGeneSymbols(colnames(gprofiler_dataset),map=current_gene_map)
+approved_gprofiler[is.na(approved_gprofiler$Suggested.Symbol),3]<-approved_gprofiler[is.na(approved_gprofiler$Suggested.Symbol),1]
 
 colnames(gprofiler_dataset)<-approved_gprofiler$Suggested.Symbol
-gprofiler_dataset<-gprofiler_dataset[,!is.na(colnames(gprofiler_dataset))]
-
 
 #sambar opravenej
 source("/media/hdc/tmp/PCAWG_somatic_germline/Jakub/data/corgenelength_mod.R") #corgenelength
